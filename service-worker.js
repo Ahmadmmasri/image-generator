10f1985d1d9a5d072231db4c30711054616e0790
Ahmadmmasri <amasri256@gmail.com>
2025-03-06 23:42:34 +0300

    init image generator

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'site-cache-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/html2canvas.min.js',
+];
+
+// Install service worker and cache essential files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate service worker and clean old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event - serve cached files when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        // Return the cached response if available
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        // Otherwise fetch from the network
+        return fetch(event.request);
+      })
+  );
+});
